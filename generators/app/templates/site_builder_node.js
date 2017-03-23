bc0d721b89bc08aa7993923312af0840a1bc92e4
Chris Scanlin <cscanlin@gmail.com>
2017-03-23 02:22:36 -0700

    add screenshot size config + refactor

--- generators/app/templates/site_builder_node.js
+++ generators/app/templates/site_builder_node.js
@@ -12,24 +12,26 @@ const config = yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf8'))
 
 class Repository {
 
-  constructor(repo_data, screenshot_target) {
+  constructor(repo_data, repo_config) {
     Object.keys(repo_data).forEach(key => {
       this[key] = repo_data[key]
     })
-    if (!repo_data.homepage && !screenshot_target) {
+    if (!repo_data.homepage && !repo_config.screenshot_target) {
       throw new Error(`Need screenshot_target or homepage for ${repo_data.name}`)
     }
-    this.screenshot_target = screenshot_target ? screenshot_target : this.homepage
-    this.homepage = this.homepage ? this.homepage : screenshot_target
+    this.screenshot_target = repo_config.screenshot_target ? repo_config.screenshot_target : this.homepage
+    this.homepage = this.homepage ? this.homepage : repo_config.screenshot_target
+    this.screenshot_width = repo_config.screenshot_width ? repo_config.screenshot_width : config.screenshot_width,
+    this.screenshot_height = repo_config.screenshot_height ? repo_config.screenshot_height : config.screenshot_height
     this.screenshot = this.screenshot_filename()
   }
 
-  static retrieve_from_url(repo_url, screenshot_target) {
-    const full_api_path = `https://api.github.com/repos${url.parse(repo_url).pathname}`
+  static retrieve(repo_config) {
+    const full_api_path = `https://api.github.com/repos${url.parse(repo_config.repo_url).pathname}`
     return fetch(full_api_path).then(response => {
       return response.json()
     }).then(repo_data => {
-      return new Repository(repo_data, screenshot_target)
+      return new Repository(repo_data, repo_config)
     })
   }
 
@@ -65,6 +67,7 @@ class Screenshotter {
     this.repositories.reduce((accumulator, repo) => {
       return accumulator.then(results => {
         return nightmare.goto(repo.screenshot_target)
+          .viewport(repo.screenshot_width, repo.screenshot_height)
           .screenshot(`${config.screenshot_directory}/${repo.screenshot_filename()}`)
           .then(result => {
             console.log(`Finished: ${repo.name}`)
@@ -93,6 +96,6 @@ class Screenshotter {
 
 }
 
-Promise.all(config.repositories.map(repo => {
-  return Repository.retrieve_from_url(repo.repo_url, repo.screenshot_target)
+Promise.all(config.repositories.map(repo_config => {
+  return Repository.retrieve(repo_config)
 })).then((repositories) => new Screenshotter(repositories).run())
